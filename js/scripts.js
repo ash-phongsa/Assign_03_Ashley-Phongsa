330176194f1199189d360aeb1380ea4535050217
ash-phongsa <117397551+ash-phongsa@users.noreply.github.com>
2022-12-12 01:41:07 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,16 @@ $("#apple-button").on("click", death)
 function foodClick() {
     foodClicks += 1;
     document.getElementById("foodClick").innerHTML = foodClicks;
-    $(".grid-dish").html("<div><img src='/imgs/meat dish.png' alt='meatbowl'></div><img src='/imgs/apple.png' alt='apple-still'>")
+    $(".grid-dish").html("<div><img src='imgs/meat dish.png' alt='meatbowl'></div><img src='imgs/apple.png' alt='apple-still'>")
     $("body").css("background-image", "url(../imgs/animals.png)")
 
     if(foodClicks >= 20) {
-        $("#apple").html("<div></div><div class='grid-dish'><div><img src='/imgs/meat dish.png'></div><img src='imgs/apple_gif.gif' alt='dance'></div>")
+        $("#apple").html("<div></div><div class='grid-dish'><div><img src='imgs/meat dish.png'></div><img src='imgs/apple_gif.gif' alt='dance'></div>")
     }
 
     if(waterClicks >= 100 && foodClicks >=100) {
         $("#page").empty()
-        $("body").css("background-image", "url(/imgs/win.png)")
+        $("body").css("background-image", "url(imgs/win.png)")
         $("#page").html("<div></div><h1>Pompom has grown into a beautiful creature due to your efforts. Thank you for giving yourself carpal tunnel.</h1>")
     }
 }
@@ -25,23 +25,23 @@ function foodClick() {
 function waterClick() {
     waterClicks += 1;
     document.getElementById("waterClick").innerHTML = waterClicks;
-    $(".grid-dish").html("<div><img src='/imgs/water dish.png' alt='waterbowl'></div><img src='/imgs/apple.png' alt='apple-still'>")
+    $(".grid-dish").html("<div><img src='imgs/water dish.png' alt='waterbowl'></div><img src='imgs/apple.png' alt='apple-still'>")
     $("body").css("background-image", "url(../imgs/rain_gif.gif)")
 
     if(waterClicks >= 20) {
-        $("#apple").html("<div></div><div class='grid-dish'><div><img src='/imgs/water dish.png'></div><img src='imgs/apple_gif.gif' alt='dance'></div>")
+        $("#apple").html("<div></div><div class='grid-dish'><div><img src='imgs/water dish.png'></div><img src='imgs/apple_gif.gif' alt='dance'></div>")
     }
 
     if(waterClicks >= 100 && foodClicks >=100) {
         $("#page").empty()
-        $("body").css("background-image", "url(/imgs/win.png)")
+        $("body").css("background-image", "url(imgs/win.png)")
         $("#page").html("<div></div><h1>Pompom has grown into a beautiful creature due to your efforts. Thank you for giving yourself carpal tunnel.</h1>")
     }
 }
 
 function death() {
     $("#page").empty()
-    $("body").css("background-image", "url(/imgs/death.png)")
+    $("body").css("background-image", "url(imgs/death.png)")
     $("#page").html("<div></div><h1>You sick freak! They were allergic to apples!!</h1>")
 }
 
